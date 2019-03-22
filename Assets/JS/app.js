0e0b3dd792fc00072fadeebf81ee624813515774
The_Costa <bryan.costa@roadrunner.com>
2019-03-21 21:20:44 -0700

    Had to redo the entire JS

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,29 +1,22 @@
-let res = []
-console.log(res);
-
-fetch(`https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple`)
-    .then(r => r.json())
-    .then(r => {
-        // r.results[]
-        console.log(r.results);
-        return r.results.push(res)
-    }
-
-        // let results = r.results
-        // results.forEach(e => {
-        //     console.log(e)
-        //     let { question } = results[`${e}`].question
-        //         document.querySelector('#trivia').innerHTML = `${[question]}`
-        // });
-        // console.log(results);
-        // r.results.forEach(i => {
-            // let { question, correct_answer, incorrect_answers } = r.results[i]
-        // document.querySelector('#trivia').innerHTML = `${r.results[r].question}`
-        )
-    .catch(e => console.error(e))
-    // }) 
-
-for (i = 0; i < res.length; i++) {
-    // let { i } = r.results
-    console.log(i)
+let questions1 = {
+    question: "Which of the following authors was not born in England? ",
+    answers: ["Arthur Conan Doyle", "Graham Greene", "H G Wells", "Arthur C Clarke"],
+    correctAnswer: 0
 }
+let questions2 = {
+    question: "What is the name of Eragon's dragon in the book series Eragon?",
+    answers: ["Saphira", "Glaedr", "Thorn", "Arya"],
+    correctAnswer: 0
+}
+let questions3 = {
+    question: `Which Author wrote the quote "Not all who wander are lost"?`,
+    answers: ["J. R. R. Tolkien", "Emily Dickinson", "Ernest Hemingway", "Mark Twain"],
+    correctAnswer: 0
+}
+let questions4 = {
+    question: "In the novel &quot;Lord of the Rings&quot;, how many rings of power were given to the race of man?",
+    answers: ["9", "5", "11", "13"],
+    correctAnswer: 0
+}
+
+
